6c0aad2ab53e3defe1fb7724472f294bba142212
Owner <ha7637@eh.amalnet.k12.il>
2025-05-06 14:42:10 +0300

    Initial commit

---
+++ app/src/main/java/com/example/catpictures/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.catpictures;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private ImageView ImgCat;
+    private RequestQueue queue;
+        @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        ImgCat = findViewById(R.id.img_cat);
+        queue = Volley.newRequestQueue(this);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        Button btn = findViewById(R.id.btn);
+        ImageView imageView = findViewById(R.id.ImgCat);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fetchCat();
+            }
+        });
+    }
+    
+}
